6b4c4b14270fae2cb8f83e465559368afc249219
mfreyre@gmail.com <mfreyre@gmail.com>
2016-04-26 16:07:30 -0400

    Mumuki Export on 2016-04-26 16:07:30 -0400

--- 00003_¿Cuáles series...?/test.hs
+++ 00003_¿Cuáles series...?/test.hs
@@ -3,8 +3,8 @@ describe "¿Cuáles series...?" $ do
     it "tieneMasDeNTemporadas 6 series  devuelve la lista [Serie \"dr house\" 8 2004 \"Universal\"]" $ do
       tieneMasDeNTemporadas 6 series `shouldBe` [Serie "dr house" 8 2004 "Universal"]
   describe "3.a.ii. posterioresAlAnio" $ do
-    it "posterioresAlAnio 2006 series devuelve la lista [Serie \"United States of Tara\" 3 2009 \"Dreamworks\", Serie \"V\",3,2009,\"Warner Bros.\", Serie \"game of thrones\" 6 2011 \"HBO\"]" $ do
-      posterioresAlAnio 2006 series `shouldBe` [Serie "United States of Tara" 3 2009 "Dreamworks", Serie "V" 3 2009 "Warner Bros.", Serie "game of thrones" 6 2011 "HBO"]
+    it "posterioresAlAnio 2006 series devuelve la lista [Serie \"United States of Tara\" 3 2009 \"Dreamworks\", Serie \"V\",3,2009,\"Warner Bros.\", Serie \"game of thrones\" 6 2011 \"HBO\", Serie \"daredevil\" 2 2015 \"Netflix\"]" $ do
+      posterioresAlAnio 2006 series `shouldBe` [Serie "United States of Tara" 3 2009 "Dreamworks", Serie "V" 3 2009 "Warner Bros.", Serie "game of thrones" 6 2011 "HBO", Serie "daredevil" 2 2015 "Netflix"]
   describe "3.a.iii. producidasPorLaCadena" $ do
     it "producidasPorLaCadena \"HBO\" series  devuelve la lista [Serie \"los soprano\" 6 1999 \"HBO\", Serie \"game of thrones\" 6 2011 \"HBO\"]" $ do
       producidasPorLaCadena "HBO" series `shouldBe` [Serie "los soprano" 6 1999 "HBO", Serie "game of thrones" 6 2011 "HBO"]
