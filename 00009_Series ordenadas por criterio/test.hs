741397ac4e90632d644aa8cc98a78f1f4e57cf87
mfreyre@gmail.com <mfreyre@gmail.com>
2016-04-26 18:49:06 -0400

    Mumuki Export on 2016-04-26 18:49:06 -0400

--- 00009_Series ordenadas por criterio/test.hs
+++ 00009_Series ordenadas por criterio/test.hs
@@ -3,3 +3,4 @@ describe "6. seriesOrdenadasPor" $ do
     seriesOrdenadasPor (menorSegun' anioComienzo) [Serie "los soprano" 7 1999 "HBO", Serie "lost" 6 2004 "ABC"] `shouldBe` True
   it "seriesOrdenadasPor (menorSegun temporadas) [Serie \"los soprano\" 7 1999 \"HBO\", Serie \"lost\" 6 2004 \"ABC\"]" $ do
     seriesOrdenadasPor (menorSegun' temporadas) [Serie "los soprano" 7 1999 "HBO", Serie "lost" 6 2004 "ABC"] `shouldBe` False
+  where menorSegun' f x y = f x < f y
