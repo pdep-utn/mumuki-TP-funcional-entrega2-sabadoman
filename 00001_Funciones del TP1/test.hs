fa8964f433cc0406a25e2cde45dd6b2492953c82
mfreyre@gmail.com <mfreyre@gmail.com>
2016-04-26 14:58:05 -0400

    Mumuki Export on 2016-04-26 14:58:05 -0400

--- 00001_Funciones del TP1/test.hs
+++ 00001_Funciones del TP1/test.hs
@@ -37,8 +37,8 @@ describe "TP1" $ do
   describe "4. quienesSonEstrellas" $ do
     it "quienesSonEstrellas actores    devuelve la lista [\"Joel Gretsch\",\"Elizabeth Mitchell\"]" $ do
       quienesSonEstrellas actores `shouldBe` ["Joel Gretsch","Elizabeth Mitchell"]
-    it "quienesSonEstrellas [Actor \"Ken Leung\" [\"lost\",\"los soprano\"],Actor \"Joel Gretsch\" [\"4400\",\"V\",\"United States of Tara\"])]    devuelve la lista [\"Joel Gretsch\"]" $ do
-      quienesSonEstrellas [Actor "Ken Leung" ["lost","los soprano"], Actor "Joel Gretsch" ["4400","V","United States of Tara"])] `shouldBe` ["Joel Gretsch"]
+    it "quienesSonEstrellas [Actor \"Ken Leung\" [\"lost\",\"los soprano\"],Actor \"Joel Gretsch\" [\"4400\",\"V\",\"United States of Tara\"]]    devuelve la lista [\"Joel Gretsch\"]" $ do
+      quienesSonEstrellas [Actor "Ken Leung" ["lost","los soprano"], Actor "Joel Gretsch" ["4400","V","United States of Tara"]] `shouldBe` ["Joel Gretsch"]
   describe "5. promedioDeTemporadas" $ do
     it "promedioDeTemporadas series   devuelve 5" $ do
       promedioDeTemporadas series `shouldBe` 5
